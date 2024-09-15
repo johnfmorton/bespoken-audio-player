a92d26a8e983a1fcd3a6beeb6dec941a7a35c643
John F Morton <morton@jmx2.com>
2024-09-15 10:02:40 -0400

    changed demo directory to _site

--- vite.demo.config.js
+++ vite.demo.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     base: 'https://johnfmorton.github.io/bespoken-audio-player/', // Set this to the GitHub Pages URL, i.e., https://<USERNAME>.github.io/<REPO>/.
     build: {
-        outDir: 'demo', // This is directory where the demo site will be built. It will be published to GitHub Pages. It is not the same as the directory where the module will be built. It also is in the .gitignore file so you will not see it in the repo.
+        outDir: '_site', // This is directory where the demo site will be built. It will be published to GitHub Pages. It is not the same as the directory where the module will be built. It also is in the .gitignore file so you will not see it in the repo.
         minify: true,
     },
 })
