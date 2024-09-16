5f1d0dfbb8e134ac9df0b882459eace44fe97935
John F Morton <morton@jmx2.com>
2024-09-16 14:18:49 -0400

    show notifications in front end for demo

--- demo-page-assets/demo.ts
+++ demo-page-assets/demo.ts
@@ -3,9 +3,78 @@
 // Import the styles for the demo page
 import './style.pcss';
 
+import Notify from 'simple-notify'
+import 'simple-notify/dist/simple-notify.css'
+import type { NotifyStatus } from 'simple-notify/dist/simple-notify'
+
 
 // Import the custom element and its initialization function
-import { BespokenAudioPlayer, initBespokenAudioPlayer } from '../lib/bespoken-audio-player';
+import { initBespokenAudioPlayer } from '../lib/bespoken-audio-player';
 
 // Initialize the custom element
 initBespokenAudioPlayer();
+
+
+const player = document.getElementById('my-audio-player');
+
+  player?.addEventListener('play', () => {
+    console.log('Event: play');
+      sendNotification({
+            title: 'Play event',
+      })
+  });
+
+  player?.addEventListener('pause', () => {
+    console.log('Event: pause');
+      sendNotification({
+            title: 'Pause event',
+      })
+  });
+
+  player?.addEventListener('ended', () => {
+    console.log('Event: ended');
+      sendNotification({
+            title: 'Ended event',
+      })
+  });
+
+  player?.addEventListener('trackChange', (event) => {
+    console.log('Event: trackChange', event.detail);
+
+      let eventDetail = !(event.detail) ? null : event.detail
+      sendNotification({
+            title: 'Track Change event',
+            text: `from #my-audio-player to track ${eventDetail.track.title || eventDetail.track.src || 'unknown'}`,
+      })
+  });
+
+  player?.addEventListener('error', (event) => {
+      const { message, trackIndex } = event.detail;
+      console.error(`Error on track ${trackIndex}:`, message);
+
+        sendNotification({
+            status: 'error',
+            title: `Error event`,
+            text: `Track ${trackIndex}: ${message}`,
+        });
+    });
+
+function sendNotification({
+    title = 'Notification',
+    status = 'info',
+    text = '',
+}: {
+    title?: string;
+    status?: NotifyStatus;
+    text?: string;
+}) {
+    new Notify({
+        showIcon: false,
+        status,
+        text,
+        title,
+        autotimeout: 4500,
+        notificationsGap: 4,
+        notificationsPadding: 1,
+    });
+}
